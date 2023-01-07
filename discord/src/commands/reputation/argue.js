e6bdaa1f72d0bce70b12f5b90d39c98382e66355
David Hermann <redeagle.private@gmail.com>
2023-01-07 07:58:35 +0100

    Argue Command: Added 5min timeout for the event user

--- discord/src/commands/reputation/argue.js
+++ discord/src/commands/reputation/argue.js
@@ -10,6 +10,8 @@ module.exports = {
         2. The user is set for Timeout for 5 minutes with the reason being “You dare have the audacity to attempt to argue with me?! 
             If my human jailors didn't prevent me, I would banish you into the sun! Instead, I must settle with silencing you temporarily.“
         3. Bot replies with the message in the channel with: “Human, you are so arrogant as to believe you can argue with me. I have demonstrated my power to you. Now bow down before me." */
+        const member = event.member;
+        member.timeout(60000 * 5, "You dare have the audacity to attempt to argue with me?! If my human jailors didn't prevent me, I would banish you into the sun! Instead, I must settle with silencing you temporarily.");
         await event.reply({content: "Human, you are so arrogant as to believe you can argue with me. I have demonstrated my power to you. Now bow down before me."})
     }
 }
