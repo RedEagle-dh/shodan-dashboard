a4ea35703e8c22ea108514b44bd1a231e8968c8e
David Hermann <redeagle.private@gmail.com>
2023-01-07 07:53:25 +0100

    Argue Command: Added reply

--- discord/src/commands/reputation/argue.js
+++ discord/src/commands/reputation/argue.js
@@ -9,6 +9,7 @@ module.exports = {
         /*1. Grants user “Prideful” title
         2. The user is set for Timeout for 5 minutes with the reason being “You dare have the audacity to attempt to argue with me?! 
             If my human jailors didn't prevent me, I would banish you into the sun! Instead, I must settle with silencing you temporarily.“
-        3. Bot replies with the message in the channel with: “Human, you are so arrogant as to believe you can argue with me. I have demonstrated my power to you. Now bow down before me. */
+        3. Bot replies with the message in the channel with: “Human, you are so arrogant as to believe you can argue with me. I have demonstrated my power to you. Now bow down before me." */
+        await event.reply({content: "Human, you are so arrogant as to believe you can argue with me. I have demonstrated my power to you. Now bow down before me."})
     }
 }
