a7a3f66f6e154ee3d193a5d44fdc09b2cd17e335
David Hermann <redeagle.private@gmail.com>
2023-01-07 07:38:35 +0100

    refactor: Edited db connection from localhost to server

--- discord/src/database/connection.js
+++ discord/src/database/connection.js
@@ -7,7 +7,13 @@ const { exit } = require('process');
 
 function getDatabase() {
     try {
-        const __Database = new Redis("redis://localhost:6379")
+        const __Database = new Redis(
+            {
+                port: process.env.REDIS_PORT || 6379,
+                host: process.env.REDIS_HOST || 'localhost',
+                password: process.env.REDIS_PW || ''
+            }
+        )
         __Log.info("Successfully connected to database!")
         return __Database
     } catch (e) {
